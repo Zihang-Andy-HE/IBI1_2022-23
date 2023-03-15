22d98134ef8aa07c5c74435cfafe54dca721e0b8
Zihang <15058618000@163.com>
2023-03-16 02:50:55 +0800

    Initialise

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,12 @@
+#start with 2 rabbits
+r = 2
+# g represent generation
+g = 1
+#stop breeding until more than 100 rabbits
+While (r <= 100) {
+#g record generation
+	g += 1
+#r multiply 2 each generation
+	r *=2
+}
+print('At '+str(g)+' generation over 100 rabbits have been born and the rabbits number is '+str(r))
