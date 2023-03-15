5afbab82c7560412c65e2f3b7b771a3927c54d4d
Zihang <15058618000@163.com>
2023-03-16 02:55:08 +0800

    Initialise

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -3,10 +3,10 @@ r = 2
 # g represent generation
 g = 1
 #stop breeding until more than 100 rabbits
-While (r <= 100) {
+while (r <= 100):
 #g record generation
 	g += 1
 #r multiply 2 each generation
 	r *=2
-}
+
 print('At '+str(g)+' generation over 100 rabbits have been born and the rabbits number is '+str(r))
