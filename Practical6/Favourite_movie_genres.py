7eedf78d5191ece79e8b4a65e43bed4793503d15
Zihang <15058618000@163.com>
2023-03-22 15:41:00 +0800

    Initialise

--- Practical6/Favourite_movie_genres.py
+++ Practical6/Favourite_movie_genres.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 #create the dictionary which contain all the data
 results_dic = {'Comedy':73,'Action':42,'Romance':38,'Fantasy':28,'Science-fiction':22,'Horror':19,'Crime':18,'Documentary':12,'History':8,'War':7}
+print(results_dic)
 # create a variable called genre of requested genre which can be modified
 genre = 'Comedy'
 # num store the numbers of students for which the given genre is their favorite
 num = results_dic[genre]
-print(num)
+print(genre + ':' + str(num))
 allgenres = 'Comedy', 'Action', 'Romance', 'Fantasy', 'Science-fiction', 'Horror', 'Crime', 'Documentary', 'History', 'War'
 allnum = [73, 42, 38, 28, 22, 19, 18, 12, 8, 7]
 #the pie plotting code obtained from https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_features.html
